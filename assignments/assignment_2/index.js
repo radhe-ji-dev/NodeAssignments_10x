962eaf036badb1d86fd054999eb89161c44590e7
Radhakant Tiwari <therktiwadi@gmail.com>
2022-01-15 10:14:37 +0530

    A2 done

--- assignments/assignment_2/index.js
+++ assignments/assignment_2/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 fs.writeFileSync("index.html", "<h1>Hello World</h1>");
-
+//process.env.PORT = 4000;
 http
   .createServer(function (req, res) {
     const data = fs.readFileSync("index.html");
@@ -11,4 +11,5 @@ http
       res.end();
     }
   })
-  .listen(3000);
+  .listen(process.env.PORT || 8080);
+console.log(process.env);

7a98bf3ffdaab361239cd721ea62e994eb1e4cb8
Radhakant Tiwari <therktiwadi@gmail.com>
2022-01-15 11:03:04 +0530

    a3 c3

--- assignments/assignment_3/app.js
+++ assignments/assignment_3/app.js
@@ -1,32 +1,34 @@
-const express=require('express');
+const express = require("express");
 const app = express();
-app.set('view engine', 'ejs')
-let users=[{name: 'John', email: 'john@gmail.com'},{name: 'Rony', email: 'tony@gmail.com'},{name:"Toy",email:"roy@gmail.com"}];
+app.set("view engine", "ejs");
+let users = [{ name: "Radhe ji", email: "radheji@gmail.com" }];
 
-// Middlewares 
+// Middlewares
 // 1. to parse incoming  application / x - www - form - urlencoded content type  to string
 // describes form data that is sent in a single block in the HTTP message body.
-app.use(express.urlencoded({
-    extended: true
-})) 
-// 2. to link public file 
-app.use(express.static(__dirname + '/public'));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+// 2. to link public file
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req,res) => {
-    res.render('homepage.ejs',{data:users});
-})
-app.get('/form', (req, res) => {
-    res.render('form.ejs');
-})
-app.post('/user/add',(req,res)=>{
-    // console.log(req.body)
-    const obj={
-        name: req.body.name,
-        email: req.body.email
-    }
-    users.push(obj)
-    res.render('homepage.ejs', { data: users });
-} );
-app.listen(3000,()=>{
-    console.log("im listening");
-})
+app.get("/", (req, res) => {
+  res.render("homepage.ejs", { data: users });
+});
+app.get("/form", (req, res) => {
+  res.render("form.ejs");
+});
+app.post("/user/add", (req, res) => {
+  // console.log(req.body)
+  const obj = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  users.push(obj);
+  res.render("homepage.ejs", { data: users });
+});
+app.listen(3000, () => {
+  console.log("listingi on port 3000");
+});
